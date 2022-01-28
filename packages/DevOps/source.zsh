6566595a0254b472f52540776a9a1538956bccfd
zzswang <zzswang@gmail.com>
2022-01-28 10:01:08 +0800

    feat(DevOps): add kr command to refresh a deployment

--- packages/DevOps/source.zsh
+++ packages/DevOps/source.zsh
@@ -3,3 +3,11 @@
 # DevOps 相关软件设置
 
 alias k=kubectl
+
+// kr namespace deployment
+function kr() {
+  namespace=$1
+  deployment=$2
+
+  kubectl -n ${namespace} patch deployment ${deployment} -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$(date +%Y%m%d%H%M)\"}}}}}"
+}
