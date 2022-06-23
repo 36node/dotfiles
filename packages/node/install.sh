f83824a46c202dc389d84ee80214a0e63d2d0d54
zzswang <zzswang@gmail.com>
2022-06-23 15:55:36 +0800

    feat(node): use version 18

--- packages/node/install.sh
+++ packages/node/install.sh
@@ -11,6 +11,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm" ] && \. "${HOMEBREW_PREFIX}/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 ## node && yarn
-nvm install 16
+nvm install 18
 npm install --global yarn
 npm install --global pnpm
